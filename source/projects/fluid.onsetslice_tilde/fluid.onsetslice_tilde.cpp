d097bed6298a23d508792e6d016c930e320895e9
Pierre Alexandre Tremblay <info@pierrealexandretremblay.com>
2019-03-04 16:58:33 +0000

    removed dangling project (onsetslice) from merge mess

--- source/projects/fluid.onsetslice_tilde/fluid.onsetslice_tilde.cpp
+++
@@ -1,9 +0,0 @@
-#include <clients/rt/OnsetSlice.hpp>
-#include <FluidMaxWrapper.hpp>
-
-
-void ext_main(void *r)
-{
-  using namespace fluid::client;
-  makeMaxWrapper<OnsetSlice<double>>("fluid.onsetslice~", OnsetParams);
-}
