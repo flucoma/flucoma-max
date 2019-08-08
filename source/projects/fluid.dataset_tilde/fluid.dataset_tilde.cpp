67081026fb9000d75dfdfe255ff2fd81a8c03620
Gerard <g.roma@hud.ac.uk>
2019-08-08 12:08:16 +0100

    change corpus_tilde to dataset_tilde

--- source/projects/fluid.dataset_tilde/fluid.dataset_tilde.cpp
+++ source/projects/fluid.dataset_tilde/fluid.dataset_tilde.cpp
@@ -1,10 +1,10 @@
 
-#include <clients/nrt/CorpusClient.hpp>
+#include <clients/nrt/DatasetClient.hpp>
 #include "FluidMaxWrapper.hpp" //nb: this include is order-sensitive because of macro name clashes in Eigen and C74
 
 
 void ext_main(void*)
 {
   using namespace fluid::client;
-  makeMaxWrapper<NRTThreadedCorpus>("fluid.corpus~");
+  makeMaxWrapper<NRTThreadedDataset>("fluid.dataset~");
 }
