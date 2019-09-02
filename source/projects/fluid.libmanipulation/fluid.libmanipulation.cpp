63ebdff7dce7e35dd6e5c733f70096edc3e46ad5
Gerard <g.roma@hud.ac.uk>
2019-09-02 11:32:15 +0100

    kdtree and kmeans objects

--- source/projects/fluid.libmanipulation/fluid.libmanipulation.cpp
+++ source/projects/fluid.libmanipulation/fluid.libmanipulation.cpp
@@ -1,6 +1,8 @@
 
 #include <clients/nrt/CorpusClient.hpp>
 #include <clients/nrt/DatasetClient.hpp>
+#include <clients/nrt/KDTreeClient.hpp>
+#include <clients/nrt/KMeansClient.hpp>
 #include "FluidMaxWrapper.hpp" //nb: this include is order-sensitive because of macro name clashes in Eigen and C74
 
 
@@ -8,5 +10,7 @@ void ext_main(void*)
 {
   using namespace fluid::client;
   makeMaxWrapper<NRTThreadedCorpus>("fluid.corpus~");
-  makeMaxWrapper<NRTThreadedDataset>("fluid.dataset~"); 
+  makeMaxWrapper<NRTThreadedDatasetClient>("fluid.dataset~");
+  makeMaxWrapper<NRTThreadedKDTreeClient>("fluid.kdtree~");
+  makeMaxWrapper<NRTThreadedKMeansClient>("fluid.kmeans~");
 }
