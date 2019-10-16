3de6051184420b16b9097566172a04fa34866961
Gerard <g.roma@hud.ac.uk>
2019-10-16 12:37:12 +0200

    add fluid.knn~ object

--- source/projects/fluid.libmanipulation/fluid.libmanipulation.cpp
+++ source/projects/fluid.libmanipulation/fluid.libmanipulation.cpp
@@ -1,11 +1,12 @@
 
 #include <clients/nrt/CorpusClient.hpp>
-#include <clients/nrt/DatasetClient.hpp>
-#include <clients/nrt/LabelsetClient.hpp>
+#include <clients/nrt/DataSetClient.hpp>
+#include <clients/nrt/LabelSetClient.hpp>
 #include <clients/nrt/KDTreeClient.hpp>
 #include <clients/nrt/KMeansClient.hpp>
 #include <clients/nrt/KNNClasClient.hpp>
 #include <clients/nrt/KNNRegClient.hpp>
+#include <clients/nrt/KNNClient.hpp>
 #include "FluidMaxWrapper.hpp" //nb: this include is order-sensitive because of macro name clashes in Eigen and C74
 
 
@@ -13,10 +14,11 @@ void ext_main(void*)
 {
   using namespace fluid::client;
   //makeMaxWrapper<NRTThreadedCorpus>("fluid.corpus~");
-  makeMaxWrapper<NRTThreadedDatasetClient>("fluid.dataset~");
-  makeMaxWrapper<NRTThreadedLabelsetClient>("fluid.labelset~");
+  makeMaxWrapper<NRTThreadedDataSetClient>("fluid.dataset~");
+  makeMaxWrapper<NRTThreadedLabelSetClient>("fluid.labelset~");
   makeMaxWrapper<NRTThreadedKDTreeClient>("fluid.kdtree~");
   makeMaxWrapper<NRTThreadedKMeansClient>("fluid.kmeans~");
   makeMaxWrapper<NRTThreadedKNNClasClient>("fluid.knnclassifier~");
   makeMaxWrapper<NRTThreadedKNNRegClient>("fluid.knnregressor~");
+  makeMaxWrapper<NRTThreadedKNNClient>("fluid.knn~");
 }
