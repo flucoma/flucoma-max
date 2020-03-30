d2617efdfcc74f5a2bd8b40f3565b8de68f22f02
Owen Green <gungwho@gmail.com>
2020-03-30 14:24:21 +0100

    Formatting

--- source/include/MaxBufferAdaptor.hpp
+++ source/include/MaxBufferAdaptor.hpp
@@ -10,46 +10,45 @@ under the European Union’s Horizon 2020 research and innovation programme
 
 
 #pragma once
-//We get lots of this warning because C74 macros, and can't (AFAICS) do anything else but mute them:
+// We get lots of this warning because C74 macros, and can't (AFAICS) do
+// anything else but mute them:
 #ifdef __clang__
 #pragma clang diagnostic ignored "-Wgnu-zero-variadic-macro-arguments"
 #endif
 
 #include <clients/common/BufferAdaptor.hpp>
 #include <data/FluidTensor.hpp>
-#include <ext_buffer.h>
 #include <atomic>
+#include <ext_buffer.h>
 
 namespace fluid {
 namespace client {
 class MaxBufferAdaptor : public BufferAdaptor
 {
 public:
-  MaxBufferAdaptor(t_object *x, t_symbol *name)
-      : mHostObject(x)
-      , mName(name)
-      , mImmediate(false)
-      , mSamps(nullptr)
-      , mBufref{buffer_ref_new(mHostObject, mName)}
-      , mLock(false)
+  MaxBufferAdaptor(t_object* x, t_symbol* name)
+      : mHostObject(x), mName(name), mImmediate(false),
+        mSamps(nullptr), mBufref{buffer_ref_new(mHostObject, mName)},
+        mLock(false)
   {}
 
   ~MaxBufferAdaptor()
   {
-    while (!tryLock());
+    while (!tryLock())
+      ;
 
     release();
     if (mBufref) object_free(mBufref);
   }
 
-  MaxBufferAdaptor(const MaxBufferAdaptor &) = delete;
-  MaxBufferAdaptor &operator=(const MaxBufferAdaptor &other) = delete;
+  MaxBufferAdaptor(const MaxBufferAdaptor&) = delete;
+  MaxBufferAdaptor& operator=(const MaxBufferAdaptor& other) = delete;
 
-  MaxBufferAdaptor(MaxBufferAdaptor &&x) noexcept { *this = std::move(x); }
+  MaxBufferAdaptor(MaxBufferAdaptor&& x) noexcept { *this = std::move(x); }
 
-  MaxBufferAdaptor &operator=(MaxBufferAdaptor &&x) noexcept
+  MaxBufferAdaptor& operator=(MaxBufferAdaptor&& x) noexcept
   {
-    if(this != &x) swap(std::move(x));
+    if (this != &x) swap(std::move(x));
     return *this;
   }
 
@@ -67,15 +66,13 @@ public:
     //      return getBuffer();
   }
 
-  void immediate(bool immediate)
+  void immediate(bool immediate) { mImmediate = immediate; }
+
+  const Result resize(index frames, index channels,
+                      double newSampleRate) override
   {
-    mImmediate = immediate;
-  }
-    
-  const Result resize(index frames, index channels, double newSampleRate) override
-  {
-    t_object *buffer = getBuffer();
-    
+    t_object* buffer = getBuffer();
+
     if (buffer)
     {
       if (!mImmediate)
@@ -86,26 +83,26 @@ public:
         t_atom args[2];
         atom_setfloat(&args[0], 0.);
         atom_setlong(&args[1], static_cast<t_atom_long>(channels));
-        t_symbol *setSizeMsg = gensym("setsize");
-        /*auto      res        = */object_method_typed(buffer, setSizeMsg, 2, args, nullptr);
+        t_symbol* setSizeMsg = gensym("setsize");
+        object_method_typed(buffer, setSizeMsg, 2, args, nullptr);
         object_method(buffer, gensym("dirty"));
         t_atom newsize;
         atom_setlong(&newsize, static_cast<t_atom_long>(frames));
-        t_symbol *sampsMsg = gensym("sizeinsamps");
+        t_symbol* sampsMsg = gensym("sizeinsamps");
         object_method_typed(buffer, sampsMsg, 1, &newsize, nullptr);
         t_atom sr;
         atom_setfloat(&sr, newSampleRate);
-        t_symbol *srMsg = gensym("sr");
-        object_method_typed(buffer,srMsg,1,&sr,nullptr);
+        t_symbol* srMsg = gensym("sr");
+        object_method_typed(buffer, srMsg, 1, &sr, nullptr);
         object_method(buffer, gensym("dirty"));
         buffer_edit_end(buffer, 1);
         lockSamps();
-        
-        if(frames != numFrames() || channels != numChans() )
+
+        if (frames != numFrames() || channels != numChans())
           return {Result::Status::kError, "Could not resize"};
-        
+
         assert(frames == numFrames() && channels == numChans());
-        
+
         return {};
       }
       else
@@ -114,21 +111,21 @@ public:
           return {Result::Status::kError, "Not enough channels in buffer"};
         if (frames > numFrames())
           return {Result::Status::kError, "Not enough space in buffer"};
-        
+
         if (sampleRate() != newSampleRate)
         {
           t_atom sr;
           atom_setfloat(&sr, newSampleRate);
-          t_symbol *srMsg = gensym("sr");
-          object_method_typed(buffer,srMsg,1,&sr,nullptr);
+          t_symbol* srMsg = gensym("sr");
+          object_method_typed(buffer, srMsg, 1, &sr, nullptr);
           object_method(buffer, gensym("dirty"));
         }
-        
+
         return {};
       }
     }
-    
-    return {Result::Status::kError,"Resize on null buffer"};
+
+    return {Result::Status::kError, "Resize on null buffer"};
   }
 
   bool acquire() const override
@@ -144,7 +141,7 @@ public:
     return false;
   }
 
-  void release() const  override
+  void release() const override
   {
     unlockSamps();
     releaseLock();
@@ -157,50 +154,63 @@ public:
     return v.col(channel);
   }
 
-  FluidTensorView<float, 1> samps(index offset, index nframes, index chanoffset) override
+  FluidTensorView<float, 1> samps(index offset, index nframes,
+                                  index chanoffset) override
   {
     FluidTensorView<float, 2> v{this->mSamps, 0, numFrames(), numChans()};
     return v(Slice(offset, nframes), Slice(chanoffset, 1)).col(0);
   }
-  
+
   FluidTensorView<const float, 1> samps(index channel) const override
   {
     FluidTensorView<const float, 2> v{this->mSamps, 0, numFrames(), numChans()};
-
     return v.col(channel);
   }
 
-  FluidTensorView<const float, 1> samps(index offset, index nframes, index chanoffset) const override
+  FluidTensorView<const float, 1> samps(index offset, index nframes,
+                                        index chanoffset) const override
   {
     FluidTensorView<const float, 2> v{this->mSamps, 0, numFrames(), numChans()};
     return v(Slice(offset, nframes), Slice(chanoffset, 1)).col(0);
   }
 
-  t_max_err notify(t_symbol *s, t_symbol *msg, void *sender, void *data) const
+  t_max_err notify(t_symbol* s, t_symbol* msg, void* sender, void* data) const
   {
-    t_symbol *buffer_name = (t_symbol *)object_method((t_object *)sender, gensym("getname"));
-    return buffer_name == mName ? buffer_ref_notify(mBufref, s, msg, sender, data) : MAX_ERR_NONE;
+    t_symbol* buffer_name =
+        (t_symbol*) object_method((t_object*) sender, gensym("getname"));
+    return buffer_name == mName
+               ? buffer_ref_notify(mBufref, s, msg, sender, data)
+               : MAX_ERR_NONE;
   }
 
-  index numFrames() const override { return valid() ? static_cast<index>(buffer_getframecount(getBuffer())) : 0; }
+  index numFrames() const override
+  {
+    return valid() ? static_cast<index>(buffer_getframecount(getBuffer())) : 0;
+  }
 
-  index numChans() const override { return valid() ? static_cast<index>(buffer_getchannelcount(getBuffer())) : 0; }
+  index numChans() const override
+  {
+    return valid() ? static_cast<index>(buffer_getchannelcount(getBuffer()))
+                   : 0;
+  }
 
-  double sampleRate() const override { return valid() ? buffer_getsamplerate(getBuffer()) : 0; }
+  double sampleRate() const override
+  {
+    return valid() ? buffer_getsamplerate(getBuffer()) : 0;
+  }
 
   std::string asString() const override { return mName->s_name; }
-  
+
   void refresh() override
   {
-    t_object *buffer = getBuffer();
-    if(buffer) object_method(buffer, gensym("dirty"));
+    t_object* buffer = getBuffer();
+    if (buffer) object_method(buffer, gensym("dirty"));
   }
-  
-private:
 
+private:
   void lockSamps() const
   {
-    t_object *buffer = getBuffer();
+    t_object* buffer = getBuffer();
     if (buffer) mSamps = buffer_locksamples(buffer);
   }
 
@@ -213,51 +223,45 @@ private:
     }
   }
 
-  bool tryLock() const
-  {
-    return compareExchange(false, true);
-  }
+  bool tryLock() const { return compareExchange(false, true); }
 
-  void releaseLock() const
-  {
-    compareExchange(true, false);
-  }
+  void releaseLock() const { compareExchange(true, false); }
 
   bool compareExchange(bool compare, bool exchange) const
   {
     return mLock.compare_exchange_strong(compare, exchange);
   }
 
-  t_object *getBuffer() const { return buffer_ref_getobject(mBufref); }
+  t_object* getBuffer() const { return buffer_ref_getobject(mBufref); }
 
-  void swap(MaxBufferAdaptor &&other) noexcept
+  void swap(MaxBufferAdaptor&& other) noexcept
   {
 
-    if(this == &other) return;
+    if (this == &other) return;
 
-    while (!tryLock());
+    while (!tryLock())
+      ;
 
     release();
     object_free(mBufref);
 
     mHostObject = other.mHostObject;
-    mName   = other.mName;
-    mSamps  = other.mSamps;
+    mName = other.mName;
+    mSamps = other.mSamps;
     mBufref = other.mBufref;
 
     other.mHostObject = nullptr;
-    other.mName   = nullptr;
-    other.mSamps  = nullptr;
+    other.mName = nullptr;
+    other.mSamps = nullptr;
     other.mBufref = nullptr;
     releaseLock();
   }
 
-  t_object *mHostObject;
-  t_symbol *mName;
-
-  bool mImmediate;
-  mutable float* mSamps;
-  t_buffer_ref* mBufref;
+  t_object*                 mHostObject;
+  t_symbol*                 mName;
+  bool                      mImmediate;
+  mutable float*            mSamps;
+  t_buffer_ref*             mBufref;
   mutable std::atomic<bool> mLock;
 };
 } // namespace client
