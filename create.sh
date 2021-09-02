1ee3f5a67c664c7cd720486e6582193aaf85f2bd
James Bradbury <jamesbradbury93@gmail.com>
2021-09-02 17:38:17 +0100

    build script

---
+++ create.sh
@@ -0,0 +1,18 @@
+mkdir -p build
+rm -r build/CMakeFiles
+rm -r build/source
+rm build/CMakeCache.txt
+rm build/Makefile
+rm build/cmake_install.cmake
+rm build/install_manifest.txt
+cd build
+
+rm -r ../externals
+rm -r ../release-packaging/Fluid\ Corpus\ Manipulation/*
+
+cmake -GNinja -DMAX_SDK_PATH=../sdk -DFLUID_PATH=../../core -DFLUID_ARCH=-mcpu=native -DCMAKE_OSX_ARCHITECTURES="arm64" ..
+ninja clean && ninja
+
+cd ..
+cd externals
+fd -e mxo -x codesign --remove-signature "{}"
