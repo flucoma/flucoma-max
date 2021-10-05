fb6e1b50ecef8ec7e7a329cb2bd92c5357738092
James Bradbury <me@jamesbradbury.net>
2021-10-05 17:04:16 +0100

    remove the euclidian distance function

--- javascript/fluid.plotter.js
+++ javascript/fluid.plotter.js
@@ -212,10 +212,6 @@ function constructColorScheme() {
 	}
 }
 
-function eudist(x1, y1, x2, y2) {
-	return Math.pow((x2-x1), 2) + Math.pow((y2-y1), 2)
-}
-
 function shape(x) {
 	_shape = x;
 	mgraphics.redraw();
