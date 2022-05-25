53c1c8d797acfc3dbdb2607064b28bdc8d1c195e
weefuzzy <gungwho@gmail.com>
2022-05-25 11:47:32 +0100

    CMake: Correct hints for Max SDK libs

--- source/script/SetupMaxSDK.cmake
+++ source/script/SetupMaxSDK.cmake
@@ -8,15 +8,15 @@
 
 include_guard() 
 
-if(MSVC)
-  foreach(flag_var
-      CMAKE_CXX_FLAGS CMAKE_CXX_FLAGS_DEBUG CMAKE_CXX_FLAGS_RELEASE
-      CMAKE_CXX_FLAGS_MINSIZEREL CMAKE_CXX_FLAGS_RELWITHDEBINFO)
-    if(${flag_var} MATCHES "/MD")
-      string(REGEX REPLACE "/MD" "/MT" ${flag_var} "${${flag_var}}")
-    endif()
-  endforeach()
-endif()
+# if(MSVC)
+#   foreach(flag_var
+#       CMAKE_CXX_FLAGS CMAKE_CXX_FLAGS_DEBUG CMAKE_CXX_FLAGS_RELEASE
+#       CMAKE_CXX_FLAGS_MINSIZEREL CMAKE_CXX_FLAGS_RELWITHDEBINFO)
+#     if(${flag_var} MATCHES "/MD")
+#       string(REGEX REPLACE "/MD" "/MT" ${flag_var} "${${flag_var}}")
+#     endif()
+#   endforeach()
+# endif()
 
 if (NOT DEFINED C74_MAX_API_DIR)
    file(TO_CMAKE_PATH "${MAX_SDK_PATH}" MAX_SDK_FULLPATH)
@@ -86,8 +86,8 @@ target_include_directories(MAX_SDK SYSTEM PUBLIC
 )
 
 if (APPLE)
-  find_library(JITTER_LIBRARY "JitterAPI" HINTS  "${C74_MSP_INCLUDES}")
-  find_library(MAX_AUDIO_API "MaxAudioAPI" HINTS "${C74_JIT_INCLUDES}")
+  find_library(JITTER_LIBRARY "JitterAPI" HINTS  "${C74_JIT_INCLUDES}")
+  find_library(MAX_AUDIO_API "MaxAudioAPI" HINTS "${C74_MSP_INCLUDES}")
 
   target_link_libraries(MAX_SDK PUBLIC
     ${JITTER_LIBRARY}
