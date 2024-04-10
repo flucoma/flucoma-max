0f68a47bb1dc97d70c370ff35afadf03c8f34009
Balint Laczko <45127463+balintlaczko@users.noreply.github.com>
2024-04-10 11:50:40 +0200

    decouple patching/pres mode position

--- javascript/fluid.jit.plotter.resize.pworld.js
+++ javascript/fluid.jit.plotter.resize.pworld.js
@@ -2,7 +2,7 @@
 // and adjusting pworld presentatin_rect accordingly
 var task = new Task(getBpatcherRect, this);
 // buffers for comparison
-var prevWidth, prevHeight;
+var prevWidthPatching, prevHeightPatching, prevWidthPresentation, prevHeightPresentation, prevPresentation;
 // Minimum and maximum intervals (in ms) for the Task.
 // It starts out at minInterval, and gradually slows
 // down to maxInterval by multiplying the current
@@ -22,35 +22,58 @@ function getBpatcherRect() {
 	if (this.patcher.box != null && forceSquareBpatcher) {
 		forceSquareAR();
 	}
-	// get bpatcher width and height
-	r = this.patcher.box.rect;
-	width = r[2] - r[0];
-	height = r[3] - r[1];
+	// get bpatcher width and height - in patching and presentation
+	patchingRect = this.patcher.box.getboxattr("patching_rect"); // [x, y, width, height]
+	presentationRect = this.patcher.box.getboxattr("presentation_rect"); // [x, y, width, height]
+	// check if we are in presentation mode
+	presentation = this.patcher.parentpatcher.getattr("presentation");
+	// get width and height
+	patchingWidth = patchingRect[2];
+	patchingHeight = patchingRect[3];
+	presentationWidth = presentationRect[2];
+	presentationHeight = presentationRect[3];
 	// output list for setting the pworld presentation_rect to width & height
-	outlet(0, 0, 0, width, height);
+	if (presentation == 1) {
+		outlet(0, 0, 0, presentationWidth, presentationHeight);
+	} else {
+		outlet(0, 0, 0, patchingWidth, patchingHeight);
+	}
 	// slow down Task refresh rate if idle
-	adjustInterval(width, height);
-	// save width & height for comparison
-	prevWidth = width;
-	prevHeight = height;
+	adjustInterval(patchingWidth, patchingHeight, presentationWidth, presentationHeight, presentation);
+	// save width & height & presentation for comparison
+	prevWidthPatching = patchingWidth;
+	prevHeightPatching = patchingHeight;
+	prevWidthPresentation = presentationWidth;
+	prevHeightPresentation = presentationHeight;
+	prevPresentation = presentation;
 }
 
 
 function forceSquareAR() {
-	// get bpatcher width and height
-	r = this.patcher.box.rect;
-	width = r[2] - r[0];
-	height = r[3] - r[1];
-	if (width == prevWidth || height == prevHeight) {
-		longestSide = width >= height ? width : height;
-		this.patcher.box.rect = [r[0], r[1], r[0]+longestSide, r[1]+longestSide];
+	// get bpatcher width and height - in patching and presentation
+	patchingRect = this.patcher.box.getboxattr("patching_rect"); // [x, y, width, height]
+	presentationRect = this.patcher.box.getboxattr("presentation_rect"); // [x, y, width, height]
+	// get width and height
+	patchingWidth = patchingRect[2];
+	patchingHeight = patchingRect[3];
+	presentationWidth = presentationRect[2];
+	presentationHeight = presentationRect[3];
+	// force it in patching
+	if (patchingWidth == prevWidthPatching || patchingHeight == prevHeightPatching) {
+		longestSide = patchingWidth >= patchingHeight ? patchingWidth : patchingHeight;
+		this.patcher.box.setboxattr("patching_rect", [patchingRect[0], patchingRect[1], longestSide, longestSide]);
+	}
+	// force it in presentation
+	if (presentationWidth == prevWidthPresentation || presentationHeight == prevHeightPresentation) {
+		longestSide = presentationWidth >= presentationHeight ? presentationWidth : presentationHeight;
+		this.patcher.box.setboxattr("presentation_rect", [presentationRect[0], presentationRect[1], longestSide, longestSide]);
 	}
 }
 
 
 // slow down Task interval when idle
-function adjustInterval(width, height) {
-	if (width == prevWidth || height == prevHeight) {
+function adjustInterval(patchingWidth, patchingHeight, presentationWidth, presentationHeight, presentation) {
+	if (patchingWidth == prevWidthPatching && patchingHeight == prevHeightPatching && presentationWidth == prevWidthPresentation && presentationHeight == prevHeightPresentation && presentation == prevPresentation) {
 		task.interval = Math.min(task.interval * intervalMult, maxInterval);
 	} else {
 		// reset to fast refresh if width or height changes
